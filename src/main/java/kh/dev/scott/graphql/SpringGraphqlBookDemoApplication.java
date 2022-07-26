6d38ac880a43cdbfcea3e859bea1c5b6cd7dd5e7
Houng Borey <Houng.Borey@sathapana.com.kh>
2022-07-26 11:52:57 +0700

    first commit

---
+++ src/main/java/kh/dev/scott/graphql/SpringGraphqlBookDemoApplication.java
@@ -0,0 +1,13 @@
+package kh.dev.scott.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlBookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlBookDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kh/dev/scott/graphql/controller/BookController.java
@@ -0,0 +1,22 @@
+package kh.dev.scott.graphql.controller;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import kh.dev.scott.graphql.model.Author;
+import kh.dev.scott.graphql.model.Book;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.getAuthorId());
+    }
+}
